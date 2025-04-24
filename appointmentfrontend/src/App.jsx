688754cc376785b4b6cb81fc228bc9b09c601eff
Jeyaruban jenushan <jjenushan550@gmail.com>
2025-04-24 08:28:37 +0530

    Login and registration form updated frontend

--- appointmentfrontend/src/App.jsx
+++ appointmentfrontend/src/App.jsx
@@ -1,9 +1,8 @@
-import React, { useContext } from "react";
+import React, { useContext, useMemo } from "react";
 import Navbar from "./components/Navbar";
 import { Routes, Route, Navigate, useLocation, Outlet } from "react-router-dom";
 import Home from "./pages/Home";
 import Doctors from "./pages/Doctors";
-
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Appointment from "./pages/Appointment";
@@ -12,33 +11,50 @@ import MyProfile from "./pages/MyProfile";
 import Footer from "./components/Footer";
 import Verify from "./pages/Verify";
 import ForgotPassword from "./pages/ForgotPassword";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+
 import { AppContext } from "./context/AppContext";
+import Register from "./pages/Register/Register";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Login from "./pages/Login/Login";
 
 const App = () => {
-  const { token } = useContext(AppContext);
   const location = useLocation();
 
-  // Check if current route is login or register
-  const isAuthPage = ["/login", "/register"].includes(location.pathname);
-
-  // Protected Route Wrapper
-  const ProtectedRoutes = () => {
-    return token ? <Outlet /> : <Navigate to="/login" replace />;
-  };
+  // Memoize the auth page check
+  const isAuthPage = useMemo(
+    () =>
+      ["/login", "/register", "/forgotPassword"].includes(location.pathname),
+    [location.pathname]
+  );
 
   return (
     <div className="mx-4 sm:mx-[10%] min-h-screen flex flex-col">
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
       {!isAuthPage && <Navbar />}
       <main className="flex-grow">
         <Routes>
           {/* Public Routes */}
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+
+          
+
           <Route path="/" element={<Home />} />
           <Route path="/doctors" element={<Doctors />} />
           <Route path="/doctors/:specialization" element={<Doctors />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/forgotPassword" element={<ForgotPassword />} />
@@ -60,4 +76,16 @@ const App = () => {
   );
 };
 
-export default App;
+// Move ProtectedRoutes outside to prevent recreation on each render
+const ProtectedRoutes = () => {
+  const { token } = useContext(AppContext);
+  const location = useLocation();
+
+  if (!token) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  return <Outlet />;
+};
+
+export default React.memo(App);
